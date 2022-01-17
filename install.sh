11e7c07da44d1e4fc4638a51a097b06dbc24d10d
Filip Troníček <filip@gitpod.io>
2022-01-17 12:03:35 +0000

    add commit signing
    Let's see how it goes

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 sudo apt-get install -y lolcat
 sudo apt install zsh
 sudo apt install git-core curl fonts-powerline
+
+[[ ! -z $GNUPG_KEY  ]] &&
+gpg --verbose --batch --import <(echo $GNUPG_KEY|base64 -d) &&
+echo 'pinentry-mode loopback' >> ~/.gnupg/gpg.conf
+git config --global commit.gpgsign true # sign all commits

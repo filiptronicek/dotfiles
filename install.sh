64c73ca16353de09506d9f690f6a53e476ea7424
Filip Troníček <filip@gitpod.io>
2022-01-17 12:15:06 +0000

    Fix up delimiters

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ sudo apt-get install -y lolcat
 sudo apt install zsh
 sudo apt install git-core curl fonts-powerline
 
-[[ ! -z $GNUPG_KEY  ]] &&
-gpg --verbose --batch --import <(echo $GNUPG_KEY|base64 -d) &&
+gpg --verbose --batch --import <(echo $GNUPG_KEY|base64 -d)
 echo 'pinentry-mode loopback' >> ~/.gnupg/gpg.conf
 git config --global commit.gpgsign true # sign all commits

8efe313c0572587ab682e922c6b5522e6818c453
Filip Troníček <filip@gitpod.io>
2022-07-12 19:59:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
-sudo apt-get install -y lolcat
 sudo apt install zsh
 sudo apt install git-core curl fonts-powerline
 
-mkdir /home/gitpod/.gitpod-code
-echo '{}' > /home/gitpod/.gitpod-code/argv.json
-
-gpg --verbose --batch --import <(echo $GNUPG_KEY|base64 -d)
-echo 'pinentry-mode loopback' >> ~/.gnupg/gpg.conf
-git config --global commit.gpgsign true # sign all commits
+npm i -g vsce

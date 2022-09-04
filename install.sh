584ac9e104af7438b56f64a0b89e81bead1b72f9
Filip Troníček <filip@gitpod.io>
2022-09-04 12:26:37 +0000

    Add tailscale setup

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 sudo apt install zsh
 sudo apt install git-core curl fonts-powerline
 
-npm i -g vsce ovsx 
+npm i -g vsce ovsx
+
+./scripts/tailscale.sh &
---
+++ scripts/tailscale.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+sudo apt-get update
+sudo apt-get install tailscale
+
+if [ -n "${TS_STATE_TAILSCALE_EXAMPLE}" ]; then
+    # restore the tailscale state from gitpod user's env vars
+    sudo mkdir -p /var/lib/tailscale
+    echo "${TS_STATE_TAILSCALE_EXAMPLE}" | sudo tee /var/lib/tailscale/tailscaled.state > /dev/null
+fi
+    sudo tailscaled &
+
+if [ -n "${TS_STATE_TAILSCALE_EXAMPLE}" ]; then
+    sudo -E tailscale up &
+else
+    sudo -E tailscale up --hostname "gitpod-${GITPOD_GIT_USER_NAME// /-}-$(echo ${GITPOD_WORKSPACE_CONTEXT} | jq -r .repository.name)" --ssh &
+    # store the tailscale state into gitpod user
+    gp env TS_STATE_TAILSCALE_EXAMPLE="$(sudo cat /var/lib/tailscale/tailscaled.state)"
+fi

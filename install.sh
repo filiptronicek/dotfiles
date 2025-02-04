d3060af5120a3a770edcf2009cd7d3607537de1e
Filip Troníček <filip@gitpod.io>
2025-02-04 20:26:08 +0000

    Add GitHub CLI authentication in Gitpod install script
    Tool: gitpod/catfood.gitpod.cloud

--- install.sh
+++ install.sh
@@ -4,4 +4,10 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source "$SCRIPT_DIR/nix.sh"
 
+if [ -n "${GITPOD_WORKSPACE_URL}" ]; then
+    # authenticate gh cli
+    GH_TOKEN=$(printf '%s\n' "host=github.com" | gp credential-helper get | awk -F'password=' '{print $2}' | tr -d '[:space:]')
+    gh auth login --with-token <<< "$GH_TOKEN"
+fi
+
 npm i -g vsce ovsx

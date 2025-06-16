e88636808598286d15c0f4dfeffcc455e0d5a804
Filip Troníček <filip@gitpod.io>
2025-06-16 11:22:58 +0200

    Auth `gh` automagically

--- install.sh
+++ install.sh
@@ -6,9 +6,14 @@ source "$SCRIPT_DIR/scripts/nix.sh"
 source "$SCRIPT_DIR/scripts/claude.sh"
 
 if [ -n "${GITPOD_WORKSPACE_URL}" ]; then
-    # authenticate gh cli
+    # authenticate gh cli for Gitpod Classic
     GH_TOKEN=$(printf '%s\n' "host=github.com" | gp credential-helper get | awk -F'password=' '{print $2}' | tr -d '[:space:]')
     gh auth login --with-token <<< "$GH_TOKEN"
+fi
+
+if [ -n "${GITPOD_ENVIRONMENT_ID}" ]; then
+    # authenticate gh cli for Gitpod
+    GH_TOKEN=$(awk -F= '/^password=/ {print $2}' /usr/local/gitpod/shared/git-secrets/*)
 fi
 
 npm i -g vsce ovsx
